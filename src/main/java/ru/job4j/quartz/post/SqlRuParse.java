673311293a943469d98f025fba912badaaba584f
paketchino <chansgorn@gmail.com>
2021-09-20 00:22:58 +0300

    2.4. SqlRuParse [#285213 #204193]

--- src/main/java/ru/job4j/quartz/post/SqlRuParse.java
+++ src/main/java/ru/job4j/quartz/post/SqlRuParse.java
@@ -4,11 +4,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.quartz.html.SqlRuParseDateTime;
 import ru.job4j.quartz.utils.DataTimeParser;
 import ru.job4j.quartz.utils.SqlDataTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,21 +22,23 @@ public class SqlRuParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
+
     @Override
     public List<Post> list(String link) {
         List<Post> list = new ArrayList<>();
-        try {
-            Document doc = Jsoup.connect(link)
-                    .get();
-            Elements msgTable = doc.select(".postslisttopic");
-            for (Element el : msgTable) {
-                Element href = el.child(0);
-                href.attr("msgTable");
-                href.text();
-                list.add(detail(link));
+        for (int i = 0; i <= 5; i++) {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(link).get();
+                Elements forumTable = doc.select(".forumTable");
+                Elements tr = forumTable.select("tr");
+                for (Element trs : tr) {
+                    Elements td = trs.select("td");
+                    list.add(detail(td.get(1).child(0).attr("href")));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return list;
     }
@@ -58,5 +62,12 @@ public class SqlRuParse implements Parse {
             e.printStackTrace();
         }
         return post;
+    }
+
+    public static void main(String[] args) {
+        SqlDataTimeParser sql = new SqlDataTimeParser();
+
+        SqlRuParse sqlRuParse = new SqlRuParse(sql);
+        sqlRuParse.list("https://www.sql.ru/forum/job-offers/1");
     }
 }
